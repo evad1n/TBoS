6e045ac07be9324554a2bdb16c340bdf3989aab0
nxb6584 <nxb6584@NM2550-06>
2017-04-19 10:21:17 -0400

    Buttons and Functions Done

--- Game1.cs
+++ Game1.cs
@@ -471,6 +471,16 @@ namespace The_Bond_of_Stone {
 			ResetGame();
 		}
 
+		public void toMainMenu() {
+			State = GameState.MainMenu;
+			Interface.ChangeMenuState(MenuState.None);
+		}
+
+		public void toHSScreen() {
+			State = GameState.MainMenu;
+			Interface.ChangeMenuState(MenuState.HighScore);
+		}
+
 		#endregion
 	}
 }
